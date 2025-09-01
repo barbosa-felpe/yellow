697d4a99ad725c47d25e3fda2bb6ce8528d934d7
barbosa-felpe <febarbosa360@gmail.com>
2025-08-31 22:12:27 -0300

    armazenamento no servidor flask

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, request, jsonify, render_template
-import json
-import os
+import json, os
 
 app = Flask(__name__)
-
-ARQUIVO_JSON = "usuario.json"
+ARQUIVO_JSON = os.path.join(os.path.dirname(__file__), "usuario.json")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
 @app.route("/cadastrar", methods=["POST"])
 def cadastrar():
-    usuario = request.get_json()
+    usuario = request.get_json(force=True)
+    print("Recebido no Flask:", usuario)
 
+    if not usuario:
+        return jsonify({"mensagem": "Erro: nenhum dado recebido"}), 400
+
+    # Cria arquivo se não existir
     if os.path.exists(ARQUIVO_JSON):
         with open(ARQUIVO_JSON, "r") as f:
             usuarios = json.load(f)
@@ -24,7 +31,7 @@ def cadastrar():
 
     with open(ARQUIVO_JSON, "w") as f:
         json.dump(usuarios, f, indent=2)
-    
+
     return jsonify({"mensagem": "Cadastro realizado com sucesso!"})
 
 if __name__ == "__main__":
