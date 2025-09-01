7404309d18efe6794bb3fa337018d74a43ae5473
Nicolas Vieira Maria <nicolasvmaria1@gmail.com>
2025-08-31 21:04:11 -0300

    banco incompleto

--- script/script.js
+++ script/script.js
@@ -14,3 +14,28 @@ window.addEventListener("scroll", function () {
       document.body.style.backgroundColor = "#ffcf00"; 
   }
 });
+
+//Informações do cliente
+
+document.getElementById("cadastro").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Pegando os valores
+    const dados = {
+      nome: document.getElementById("nome").value,
+      senha: document.getElementById("senha").value,
+      telefone: document.getElementById("telefone").value,
+      email: document.getElementById("email").value,
+      cpf: document.getElementById("cpf").value,
+    };
+
+    // Enviando pro servidor
+    const resposta = await fetch("http://localhost:3000/cadastrar", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dados),
+    });
+
+    const resultado = await resposta.json();
+    alert(resultado.mensagem);
+  });
