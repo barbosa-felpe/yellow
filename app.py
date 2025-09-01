116b7a538303912683aa8abf94e8a7223f8aa24d
Nicolas Vieira Maria <nicolasvmaria1@gmail.com>
2025-09-01 14:11:24 -0300

    verifica cpf

--- app.py
+++ app.py
@@ -4,35 +4,46 @@ import json, os
 app = Flask(__name__)
 ARQUIVO_JSON = os.path.join(os.path.dirname(__file__), "usuario.json")
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/cadastro")
 def cadastro():
     return render_template("cadastro.html")
 
+
 @app.route("/cadastrar", methods=["POST"])
 def cadastrar():
-    usuario = request.get_json(force=True)
-    print("Recebido no Flask:", usuario)
-    
+    novo_usuario = request.get_json(force=True)  # renomeei p/ evitar conflito
+    print("Recebido no Flask:", novo_usuario)
 
-    if not usuario:
+    if not novo_usuario:
         return jsonify({"mensagem": "Erro: nenhum dado recebido"}), 400
 
+    # Carregar os usuários já existentes
     if os.path.exists(ARQUIVO_JSON):
         with open(ARQUIVO_JSON, "r") as f:
             usuarios = json.load(f)
     else:
         usuarios = []
 
-    usuarios.append(usuario)
+    # Verificar se já existe CPF cadastrado
+    for usuario in usuarios:
+        if usuario["cpf"] == novo_usuario["cpf"]:
+            return jsonify({"erro": "CPF já cadastrado"}), 400
 
+    # Se não existir, adiciona
+    usuarios.append(novo_usuario)
+
+    # Salva no arquivo JSON
     with open(ARQUIVO_JSON, "w") as f:
         json.dump(usuarios, f, indent=2)
 
     return jsonify({"mensagem": "Cadastro realizado com sucesso!"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
